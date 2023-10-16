6af2dd5374d4a5a16a18cbed76704550bbdd94e6
Sabrina <bakirova200024@gmail.com>
2023-10-16 17:50:33 +0600

    check the 3 route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module test
 
 go 1.20
+
+require github.com/scorredoira/email v0.0.0-20191107070024-dc7b732c55da // indirect
--- internal/handlers/sendFile.go
+++ internal/handlers/sendFile.go
@@ -1,13 +1,15 @@
 package handlers
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
 	"net/smtp"
+	"os"
 	"strings"
+
+	"github.com/scorredoira/email"
 )
 
 func (h *ArchiveHandler) SendFileHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,6 +17,13 @@ func (h *ArchiveHandler) SendFileHandler(w http.ResponseWriter, r *http.Request)
 		// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		errorHandler(w, r, http.StatusMethodNotAllowed, "")
 
+		return
+	}
+
+	// Create the "uploads" directory if it doesn't exist
+	if err := os.MkdirAll("uploads", os.ModePerm); err != nil {
+		fmt.Println(err)
+		http.Error(w, "Unable to create 'uploads' directory", http.StatusInternalServerError)
 		return
 	}
 
@@ -43,16 +52,77 @@ func (h *ArchiveHandler) SendFileHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Send the file to the specified emails
-	err = sendFileToEmails(file, fileHeader.Filename, emails)
+	// Create a temporary file to store the uploaded file
+	tempFile, err := os.CreateTemp("uploads", "upload-*.txt")
 	if err != nil {
-		http.Error(w, "Failed to send file", http.StatusInternalServerError)
+		fmt.Println(err)
+		http.Error(w, "Unable to create temporary file", http.StatusInternalServerError)
+		return
+	}
+	defer tempFile.Close()
+
+	// Copy the file to the temporary file
+	_, err = io.Copy(tempFile, file)
+	if err != nil {
+		http.Error(w, "Error copying file", http.StatusInternalServerError)
+		return
+	}
+
+	// Send email with the file attachment
+	err = sendFileToEmails(fileHeader.Filename, tempFile.Name(), emails)
+	if err != nil {
+		http.Error(w, "Error sending email", http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "File sent successfully")
 }
+
+const (
+	smtpServer   = "smtp.gmail.com"
+	smtpPort     = "587"
+	smtpUsername = "bakirova200024@gmail.com"
+	smtpPassword = "zevghlaxkgwpkdic"
+)
+
+// Send the file to the specified emails using SMTP
+func sendFileToEmails(fileName, filePath string, emails []string) error {
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", smtpUsername, smtpPassword, smtpServer)
+
+	// Create an email message.
+	m := email.NewMessage("Subject", "Body")
+
+	// Add attachments.
+	if err := m.Attach(filePath); err != nil {
+		panic(err)
+	}
+
+	// if err := m.AttachFile("path/to/file2.jpg"); err != nil {
+	// 	panic(err)
+	// }
+
+	// Send the email.
+	err := email.Send("smtp.example.com:587", auth, m)
+	if err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+// Send the email
+// err := smtp.SendMail(
+// 	"smtp.gmail.com:587",
+// 	auth,
+// 	"bakirova200024@gmail.com",
+// 	[]string{"bakirova200024@gmail.com"},
+// 	// emails,
+// 	buffer.Bytes(),
+// )
+// if err != nil {
+// 	fmt.Println(err)
+// }
 
 // Check if the file MIME type is valid
 func isValidFileType(fileHeader *multipart.FileHeader) bool {
@@ -66,52 +136,45 @@ func isValidFileType(fileHeader *multipart.FileHeader) bool {
 	return allowedTypes[fileHeader.Header.Get("Content-Type")]
 }
 
-// Send the file to the specified emails using SMTP
-func sendFileToEmails(file io.Reader, filename string, emails []string) error {
-	// Replace these with your actual email credentials
-	from := "bakirova200024@gmail.com"
-	password := "54321bsb"
-	smtpServer := "smtp.gmail.com"
-	smtpPort := "587"
+// // Set up the MIME headers
+// headers := make(map[string]string)
+// headers["From"] = smtpUsername
+// headers["To"] = strings.Join(emails, ", ")
+// headers["Subject"] = "File Attachment"
 
-	auth := smtp.PlainAuth("", from, password, smtpServer)
+// // Create the MIME message
+// var message bytes.Buffer
 
-	// Create the email message
-	// message := createMessage(from, emails, "File Subject", "File Body")
+// for key, value := range headers {
+// 	message.WriteString(fmt.Sprintf("%s: %s\r\n", key, value))
+// }
 
-	var body bytes.Buffer
-	writer := multipart.NewWriter(&body)
+// // Attach the file
+// message.WriteString("MIME-version: 1.0\r\n")
+// message.WriteString("Content-Type: multipart/mixed; boundary=foo\r\n\r\n")
 
-	// Add the file part to the message
-	part, err := writer.CreateFormFile("file", filename)
-	if err != nil {
-		return err
-	}
+// message.WriteString("--foo\r\n")
+// message.WriteString(fmt.Sprintf("Content-Type: text/plain\r\n\r\n"))
+// message.WriteString("Please find the attached file.\r\n\r\n")
 
-	_, err = io.Copy(part, file)
-	if err != nil {
-		return err
-	}
+// message.WriteString("--foo\r\n")
+// message.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=\"%s\"\r\n", fileName))
+// message.WriteString("Content-Type: application/octet-stream\r\n\r\n")
 
-	// Add the emails part to the message
-	writer.WriteField("emails", strings.Join(emails, ","))
+// // Read the file content
+// fileContents, err := os.ReadFile(filePath)
+// if err != nil {
+// 	return err
+// }
 
-	writer.Close()
+// message.Write(fileContents)
+// message.WriteString("\r\n\r\n--foo--\r\n")
 
-	// Send the email with the file attachment
-	err = smtp.SendMail(smtpServer+":"+smtpPort, auth, from, emails, body.Bytes())
-	if err != nil {
-		return err
-	}
+// // Connect to the server, authenticate, and send the email
+// err = smtp.SendMail(fmt.Sprintf("%s:%s", smtpServer, smtpPort), auth, smtpUsername, emails, message.Bytes())
+// if err != nil {
+// 	return err
+// }
 
-	return nil
-}
-
-// Create the email message
-func createMessage(from string, to []string, subject string, body string) string {
-	message := "From: " + from + "\n" +
-		"To: " + strings.Join(to, ",") + "\n" +
-		"Subject: " + subject + "\n\n" +
-		body
-	return message
-}
+// return nil
+// }
